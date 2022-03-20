71da32169743c4b7b94489e8f12f4c4d7cb320cb
psmyrdek <przemek.smyrdek@gmail.com>
2022-03-20 19:15:12 +0100

    Add spaceships

--- src/utils/buildStage.js
+++ src/utils/buildStage.js
@@ -1,5 +1,13 @@
 import {reactive} from "vue";
 
+function addSpaceship(stage, startRow, startCol) {
+    stage[startRow + 1][startCol] = true
+    stage[startRow + 2][startCol + 1] = true
+    stage[startRow + 2][startCol + 2] = true
+    stage[startRow][startCol + 2] = true
+    stage[startRow + 1][startCol + 2] = true
+}
+
 export function buildStage(rows, cols) {
     const game = reactive({
         stage: []
@@ -12,6 +20,10 @@ export function buildStage(rows, cols) {
         }
         game.stage.push(row)
     }
+
+    /* Modify initial setup */
+    addSpaceship(game.stage, 0, 0)
+    addSpaceship(game.stage, 20, 20)
 
     return game
 }
